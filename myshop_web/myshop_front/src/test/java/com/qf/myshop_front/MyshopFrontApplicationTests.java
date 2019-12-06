a72b0e663e5d0b34680b8c0edb36247d86def4c8
ruanhao <test@qq.com>
2019-12-06 16:21:21 +0800

    添加前台页面 后台增加mybatis-plus分页插件 实现简单分页查询

---
+++ myshop_web/myshop_front/src/test/java/com/qf/myshop_front/MyshopFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.myshop_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshopFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
